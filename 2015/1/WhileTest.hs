856368e4d83fd6dd8929dca0a094e9267981a0f0
Dmitry Boulytchev <dboulytchev@gmail.com>
2015-12-24 14:29:13 +0300

    Fixing integration

--- 2015/1/WhileTest.hs
+++ 2015/1/WhileTest.hs
@@ -8,14 +8,14 @@ testList (f:fs) = f (testList fs)
 
 main = do
   let a1 = testList [(\ _ -> let r = result (int isPrime [n]) in
-                            let p = prime n in
-                            if (r == 1 && p) || (r == 0) && not p then ()
-                                                                  else error $ "Prime error: isPrime (" ++ show n ++ ") /= " ++ show p
+                             let p = prime n in
+                             if (r == 1 && p) || (r == 0) && not p then ()
+                                                                   else error $ "Prime error: isPrime (" ++ show n ++ ") /= " ++ show p
                     ) | n <- [1..100]]
   let a2 = testList [(\ _ -> let r = result (int fact [n]) in
-                            let p = fact' n in
-                            if p == r then () 
-                                      else error $ "Fact error: fact (" ++ show n ++ ") /= " ++ show p
+                             let p = fact' n in
+                             if p == r then () 
+                                       else error $ "Fact error: fact (" ++ show n ++ ") /= " ++ show p
                     )| n <- [1..8]]
   putStrLn $ show a1 ++ show a2  
   putStrLn "**********************"
