dc6ccc4e475345e06816462a030b693a876fda21
Dmitry Boulytchev <dboulytchev@gmail.com>
2015-12-24 14:25:58 +0300

    Fixed tests

--- 2015/1/WhileTest.hs
+++ 2015/1/WhileTest.hs
@@ -1,19 +1,5 @@
 import While
 
-isPrime =
-  READ "n" :>>:
-  IF (X "n" :<: C 4) 
-     (WRITE (C 1))
-     ("d" ::=: C 2 :>>:
-      "cont" ::=: C 1 :>>:
-      WHILE ((X "n" :>: X "d" :*: X "d" :\/: X "n" :=: X "d" :*: X "d") :/\: X "cont")
-            (IF (X "n" :%: X "d" :=: C 0)
-                ("cont" ::=: C 0)
-                ("d"    ::=: X "d" :+: C 1)
-            ) :>>:
-      WRITE (X "cont")
-     )
-
 result (Left s)    = error $ "Failed: " ++ s
 result (Right [n]) = n
 
@@ -21,16 +7,17 @@ testList []     = ()
 testList (f:fs) = f (testList fs)
 
 main = do
-  let _ = testList [(\ _ -> let r = result (int isPrime [n]) in
+  let a1 = testList [(\ _ -> let r = result (int isPrime [n]) in
                             let p = prime n in
                             if (r == 1 && p) || (r == 0) && not p then ()
                                                                   else error $ "Prime error: isPrime (" ++ show n ++ ") /= " ++ show p
                     ) | n <- [1..100]]
-  let _ = testList [(\ _ -> let r = result (int fact [n]) in
+  let a2 = testList [(\ _ -> let r = result (int fact [n]) in
                             let p = fact' n in
                             if p == r then () 
                                       else error $ "Fact error: fact (" ++ show n ++ ") /= " ++ show p
-                    )| n <- [1..8]]  
+                    )| n <- [1..8]]
+  putStrLn $ show a1 ++ show a2  
   putStrLn "**********************"
   putStrLn "** All tests passed **"
   putStrLn "**********************"
