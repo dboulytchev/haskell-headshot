7a94aa3f7dea5ae6a1019649e278b57e18940dd4
SlesarevIlya <slesarev.pr@gmail.com>
2015-12-24 17:15:41 +0300

    Слесарев Илья

--- 2015/1/Graph.hs
+++ 2015/1/Graph.hs
@@ -1,5 +1,7 @@
 module Graph where
 
+import Data.List
+
 -- График целочисленной функции f --- это список пар (x, y), такой, что 
 -- f (x) = y <=> пара (x, y) входит в этот список. Гарантируется, что
 -- в списке нет двух пар с одинаковыми первыми компонентами; никаких
@@ -10,7 +12,7 @@ newtype Graph = G {unG :: [(Int, Int)]} deriving Show
 -- fromFun f m n строит график функции f в области определения от
 -- m до n включительно c шагом 1.
 fromFun :: (Int -> Int) -> Int -> Int -> Graph
-fromFun = undefined
+fromFun f m n = G { unG = [ (x, f x) | x <- [m..n] ] }
 
 -- toFun получает график и возвращает функцию. 
 toFun :: Graph -> (Int -> Int)
@@ -18,35 +20,39 @@ toFun = undefined
 
 -- Графики можно сравнивать на равенство
 instance Eq Graph where
-  (==) = undefined
+  (==) a b = unG a == unG b
 
 -- Графики упорядочены по теоретико-множественному включению
 instance Ord Graph where
-  (<=) = undefined
+  (<=) a b = foldl (&&) True [elem x $ unG b | x <- unG a]
 
 -- dom g возвращает область определения графика
 dom :: Graph -> [Int]
-dom = undefined
+dom f = [x | (x, _) <- unG f]
 
 -- compose g1 g2 возвращает график суперпозиции функций с графиками
 -- g1 и g2 (сначала применяется g1, потом g2)
 compose :: Graph -> Graph -> Graph
-compose = undefined
+compose _ (G []) = G []
+compose graph1 graph2 = G [(fst x, (toFun graph2) $ snd x) | x <- unG graph1]
   
 -- restrict g l строит сужение графика g на l. Не предполагается,
 -- что l --- подмножество dom g.
 restrict :: Graph -> [Int] -> Graph
-restrict = undefined
+restrict graph l = G [(x, toFun graph x) | x <- l `intersect` fst (unzip $ unG graph)]
 
 -- isIncreasing g == True <=> g --- график (нестрого) возрастающей функции
 isIncreasing :: Graph -> Bool
-isIncreasing = undefined
-
+isIncreasing graph = length (filter (\(y1, y2) -> y1 < y2) (zip ys $ tail ys)) == (length graph' - 1) where
+  graph' = sort $ unG graph
+  ys = snd $ unzip graph'
+  
 -- isInjective g == True <=> g --- график инъективной функции
 isInjective :: Graph -> Bool
-isInjective = undefined
+isInjective graph = length (nub list) == length list where
+  list = snd $ unzip $ unG graph
 
 -- areMutuallyInverse g1 g2 == True <=> g1 и g2 --- графики взаимно-обратных
 -- функций
 areMutuallyInverse :: Graph -> Graph -> Bool
-areMutuallyInverse = undefined
+areMutuallyInverse graph1 graph2 = sort (unG graph1) == sort [(y, x) | (x, y) <- unG graph2]
