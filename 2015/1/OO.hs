69e7661a93dce4304559b280eb3e7cd187bffc70
ksmirenko <k.smirenko@gmail.com>
2015-12-24 18:15:08 +0300

    Implemented OO task.

--- 2015/1/OO.hs
+++ 2015/1/OO.hs
@@ -1,6 +1,15 @@
 -- Дописать этот текст так, чтобы написанное ниже
 -- компилировалось и работало
 
+data Point = Point (Int, Int)
+
+new = ($)
+(->>) obj func = func obj
+getX (Point (x, _)) = x
+getY (Point (_, y)) = y
+setX x (Point (_, y)) = Point (x, y)
+setY y (Point (x, _)) = Point (x, y)
+
 main = do
   let p0 = new Point (2, 3)  
       x  = p0->>getX
