4cbb5878344acacba6baa83c29380fed21e77abc
SergeevED <sergeev.ev.dm@gmail.com>
2015-12-24 17:01:44 +0300

    Update Graph.hs

--- 2015/1/Graph.hs
+++ 2015/1/Graph.hs
@@ -1,3 +1,4 @@
+
 module Graph where
 
 import Data.List
@@ -13,46 +14,24 @@ findFx x = find (\(e1, e2) -> e1 == x)
 toFun :: Graph -> (Int -> Int)
 toFun (G g) = (\x -> case find (\(e1, e2) -> e1 == x) g of
 						Just (x1, x2) -> x2)
-					
-minX (G g) =  minimum [x | (x, y) <- g]
+	
+minX (G g) = minimum [x | (x, y) <- g]
 maxX (G g) = maximum [x | (x, y) <- g]
 
 instance Eq (Graph) where
-  (==) (G g1) (G g2) = 
-	let min1 = minimum ([x | (x, y) <- g1] ++ [x | (x, y) <- g2]) in
-			isEq (G g1) (G g2) min1 where
-			isEq (G g1) (G g2) x | x > maximum ([x | (x, y) <- g1] ++ [x | (x, y) <- g2]) = True
-			isEq (G g1) (G g2) x = 
-							if ((find (\(e1, e2) -> e1 == x) g1) == Nothing) && ((find (\(e1, e2) -> e1 == x) g2) == Nothing) 
-							then isEq (G g1) (G g2) (x + 1)
-							else
-								if (find (\(e1, e2) -> e1 == x) g2) == Nothing 
-								then False
-								else (findFx x g1 == findFx x g2) && (isEq (G g1) (G g2) (x + 1))
+  (==) (G g1) (G g2) = sort g1 == sort g2
 							
 instance Ord Graph where
-  (<=) (G g1) (G g2) = 
-	let min1 = minimum ([x | (x, y) <- g1]) in
-		---max1 = maximum [x | (x, y) <- g2] in
-			isEq (G g1) (G g2) min1 where
-			isEq (G g1) (G g2) x | x > maximum ([x | (x, y) <- g1]) = True
-			isEq (G g1) (G g2) x = 
-								if ((find (\(e1, e2) -> e1 == x) g1) == Nothing) && ((find (\(e1, e2) -> e1 == x) g2) == Nothing) 
-								then isEq (G g1) (G g2) (x + 1)
-								else
-									if (find (\(e1, e2) -> e1 == x) g2) == Nothing 
-									then False
-									else (findFx x g1 == findFx x g2) && (isEq (G g1) (G g2) (x + 1))
-								
+  (<=) (G []) (G []) = True
+  (<=) (G g1) (G g2) = let i = nub [ elem x g2 | x <- g1] in 
+					(head i && length i == 1) 
+  
 dom :: Graph -> [Int]
-dom (G g) = foldl (\acc (x, y) -> if (find (\(e1, e2) -> e1 == x) g) /= Nothing then x : acc else acc) [min1..max1] g where
-	min1 = minX (G g)
-	max1 = maxX (G g)
+dom (G g) = [x | (x, y) <- g]
 			
 compose :: Graph -> Graph -> Graph
-compose (G g1) (G g2) = G $ [(x, f (G g2) y)| (x, y) <- g1] where
-	f (G g) y = case (find (\(e1, e2) -> e1 == y) g2) of
-					Just (a1, a2) -> a2
+compose (G g1) (G []) = G []
+compose (G g1) (G g2) = (G [ (fst x, toFun (G g2) (snd x)) | x <- g1])
 					
 restrict :: Graph -> [Int] -> Graph
 restrict (G g) l = G [(x, f x) | x <- l, (find (\(e1, e2) -> e1 == x) g) /= Nothing] where
@@ -62,7 +41,7 @@ restrict (G g) l = G [(x, f x) | x <- l, (find (\(e1, e2) -> e1 == x) g) /= Noth
 isIncreasing :: Graph -> Bool
 isIncreasing (G g) = 
 	let l = [y | (x, y) <- (sort g)] in
-		all (>=0) (zipWith (-) l (reverse l))
+		all (<=0) (zipWith (-) l (tail l))
 		
 isInjective :: Graph -> Bool
 isInjective (G g) = 
@@ -70,4 +49,4 @@ isInjective (G g) =
 		length l == length (nub l)
 		
 areMutuallyInverse :: Graph -> Graph -> Bool
-areMutuallyInverse (G g1) (G g2) = g1 == [ (snd x, fst x) | x <- g2]
+areMutuallyInverse (G g1) (G g2) = g1 == [ (snd x, fst x) | x <- g2] 
