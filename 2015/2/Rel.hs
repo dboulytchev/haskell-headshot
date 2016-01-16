f88377961c8dbd5dcd659e931b2e603c7737d383
AnnaAK <anna.kudryashova.va@gmail.com>
2016-01-16 13:51:28 +0300

    task for 3

--- 2015/2/Rel.hs
+++ 2015/2/Rel.hs
@@ -9,50 +9,55 @@ newtype R = R {unR :: [(Int, Int)]} deriving Show
 
 -- Реализовать сравнение отношений на равенство (как множеств)
 instance Eq R where
-  (==) = undefined
+  (==) r1 r2 = (sort $ unR r1) == (sort $ unR r2)
 
 -- sub r1 r2 == True <=> r1 --- подмножество r2
 sub :: R -> R -> Bool
-sub = undefined
+sub (R []) (R []) = True
+sub r1 r2 = (head answ && length answ == 1) 
+							where answ = nub [elem x (unR r2) | x <- (unR r1)] 
 
 -- dom r --- список (без повторений) целых чисел, каждое из
 -- которых имеет хотя бы одно вхождение в r
 dom :: R -> [Int]
-dom = undefined
+dom r = nub $ concat [[x,y] | x <- fst $ unzip $ unR r, y <- snd $ unzip $ unR r]
 
 -- add r1 r2 --- объединение r1 и r2 как множеств (в результате
 -- должен получиться список без повторений)
 add :: R -> R -> R
-add = undefined
+add (R[]) (R[]) = R[]
+add r1 r2 = R {unR = [(fst x, snd y) | x <- unR r1, y <- unR r2, snd x == snd y && fst x == fst y]}
 
 -- rev r --- отношение, "обратное" к r (т.е. в каждой паре надо
 -- компоненты поменять местами).
 rev :: R -> R
-rev = undefined
+rev r = R {unR = [(snd x, fst x) | x <- unR r]}
 
 -- join r1 r2 --- отношение, состоящее из тех и только тех пар
 -- (x, y), для которых существует z, такое, что (x, z) принадлежит
 -- r1 и (z, y) принадлежит r2
 join :: R -> R -> R
-join = undefined
+join r1 r2 = R {unR = [(fst x, snd y) | x <- unR r1, y <- unR r2, snd x == fst y]}
 
 -- closure r строит транзитивное замыкание отношения r (т.е. наименьшее
 -- транзитивное отношение, содержащее r)
 closure :: R -> R
-closure = undefined
+--closure (R r) = R(r ++ unR((join (R r) (R r)))) 
+closure r = R (nub (unR r ++ unR(join r r)))
 
 -- isReflexive r == True <=> r --- рефлексивное отношение на dom r
 isReflexive :: R -> Bool
-isReflexive = undefined
+isReflexive r =  sort ([(fst x, snd x) | x <- (unR r), snd x == fst x ]) == sort (unR r) && ([(fst x, snd x) | x <- (unR r), fst x `elem` dom r]) /= []
 
 -- isSymmetric r == True <=> r --- симметрияное отношение
 isSymmetric :: R -> Bool
-isSymmetric = undefined
+isSymmetric r = sort ([(snd x, fst x) | x <- (unR r)]) == sort (unR r) && (length (unR r)) `mod` 2 == 0
 
 -- isTransitive r == True <=> r --- транзитивное отношение
 isTransitive :: R -> Bool
-isTransitive = undefined
+isTransitive r = closure r  ==  r 
+
 
 -- isEquivalence r == True <=> r --- отношение эквивалентности
 isEquivalence :: R -> Bool
-isEquivalence = undefined
+isEquivalence r = isReflexive r && isSymmetric r && isTransitive r
