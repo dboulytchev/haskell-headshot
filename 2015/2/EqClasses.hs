d90de14d770203e9703c3afbff71684a411ee221
Zarina Kurbatova <onewhl@users.noreply.github.com>
2016-01-16 16:26:52 +0300

    Update EqClasses.hs

--- 2015/2/EqClasses.hs
+++ 2015/2/EqClasses.hs
@@ -5,9 +5,6 @@ import Rel
 -- Написать функцию, которая по отношению (см. файл Rel.hs), которое
 -- является отношением эквивалентности, строит список классов эквивалентности
 classes :: R -> [[Int]]
-classes r = map nub $ foldl classEq [] (unR r) 
-			where 
-				сlassEq [] (a,b) = [[a, b]] 
-				classEq l@(y:ys) (a, b) = if (a `elem` y || b `elem` y) 
-					then (a:b:y):ys
-					else [a, b]:l
+classes r = map nub $ foldl classEq [] (unR r) where 
+	сlassEq [] (a,b) = [[a, b]] 
+	classEq l@(y:ys) (a, b) = if (a `elem` y || b `elem` y) then (a:b:y):ys else [a, b]:l
