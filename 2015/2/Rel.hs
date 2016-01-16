25d3b94226c89c1c30afa181a7dcf5b90339fb25
minonastya <minovskayanastya@gmail.com>
2016-01-16 15:40:41 +0300

    Update Rel.hs

--- 2015/2/Rel.hs
+++ 2015/2/Rel.hs
@@ -9,7 +9,7 @@ newtype R = R {unR :: [(Int, Int)]} deriving Show
 
 -- Реализовать сравнение отношений на равенство (как множеств)
 instance Eq R where
-  (R r1) == (R r2) = (and [elem x r2|x <- r1]) && (and [elem y r1| y<-r2])
+  (R r1) == (R r2) = (and [elem x r2|x <- r1]) && (and [elem y r1| y <- r2])
 
 -- sub r1 r2 == True <=> r1 --- подмножество r2
 sub :: R -> R -> Bool
@@ -19,7 +19,7 @@ sub (R r1) (R r2) = and [elem x r2| x <- r1]
 -- которых имеет хотя бы одно вхождение в r
 dom :: R -> [Int]
 dom (R r) = foldl (\acc x -> if not(elem x acc) then (x:acc) else acc)  [] r'
- where r' = foldl (\acc x -> (fst x):(snd x):acc) [] r
+	where r' = foldl (\acc x -> (fst x):(snd x):acc) [] r
 
 -- add r1 r2 --- объединение r1 и r2 как множеств (в результате
 -- должен получиться список без повторений)
@@ -35,18 +35,15 @@ rev (R r) = R(map (\x -> (snd x, fst x)) r)
 -- (x, y), для которых существует z, такое, что (x, z) принадлежит
 -- r1 и (z, y) принадлежит r2
 join :: R -> R -> R
-join (R r1) (R r2) = R([(fst' x, snd' x)|x<-s])
+join (R r1) (R r2) = R([(fst x, snd x)|x<-s])
 	where 
-		s = [(x,y,z1)| (x,z1)<- r1, (z2,y)<-r2, z1 == z2 ]
-		fst' (x,_,_) = x
-		snd' (_,y,_) = y
-	
+		s = [(x,y)| (x,z1)<- r1, (z2,y)<-r2, z1 == z2 ]	
 	
 -- closure r строит транзитивное замыкание отношения r (т.е. наименьшее
 -- транзитивное отношение, содержащее r)
 closure :: R -> R
-closure (R r) = add y (join y y)
-	where y = R (foldl (\acc x -> if (elem x (unR(join (R r) (R r)))) then acc else x:acc) [] r)
+closure (R r) = if ((R r) == nr) then  nr else closure nr
+	where nr = add (R r) (join (R r) (R r))
 
 -- isReflexive r == True <=> r --- рефлексивное отношение на dom r
 isReflexive :: R -> Bool
@@ -58,7 +55,7 @@ isSymmetric (R r) = and[elem (x,y) r|(y,x) <- r]
 
 -- isTransitive r == True <=> r --- транзитивное отношение
 isTransitive :: R -> Bool
-isTransitive (R r) = sub (closure (R r)) (R r)
+isTransitive (R r) = (closure (R r)) == (R r)
 
 -- isEquivalence r == True <=> r --- отношение эквивалентности
 isEquivalence :: R -> Bool
