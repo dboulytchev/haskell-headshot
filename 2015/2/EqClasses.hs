d675cf4bdd7cc943c9a97221709251418a6fba2e
Sharganov Artem <sharganov.artem@yandex.ru>
2016-01-16 13:29:55 +0300

    finished task

--- 2015/2/EqClasses.hs
+++ 2015/2/EqClasses.hs
@@ -4,5 +4,8 @@ import Rel
 
 -- Написать функцию, которая по отношению (см. файл Rel.hs), которое
 -- является отношением эквивалентности, строит список классов эквивалентности
-classes :: R -> [[Int]]
-classes = undefined
+classes r = map nub $ foldl in_class [] (unR r)
+      where
+ 	 in_class [] (a,b) = [[a, b]]
+ 	 in_class list@(y:ys) (a, b) = if (elem a y || elem b y) then (a:b:y):ys else [a, b]:list
+	 
