45fcf02ece44f8c7c36e8f1b237a4bc185076640
Anna Maryina <anyutamaryina@yandex.ru>
2016-01-16 11:56:46 +0300

    EqClasses

--- 2015/2/EqClasses.hs
+++ 2015/2/EqClasses.hs
@@ -1,8 +1,15 @@
 module EqClasses where
 
+import Data.List
 import Rel
 
 -- Написать функцию, которая по отношению (см. файл Rel.hs), которое
 -- является отношением эквивалентности, строит список классов эквивалентности
 classes :: R -> [[Int]]
-classes = undefined
+classes (R r) = nub $ map (\x -> relWith (R r) x) $ flat r
+
+flat :: [(Int, Int)] -> [Int]
+flat l = sort $ nub $ uncurry (++) $ unzip l
+
+relWith :: R -> Int -> [Int]
+relWith (R r) x = flat $ filter (\(a, b) -> a == x || b == x) r
