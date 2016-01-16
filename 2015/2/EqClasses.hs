fb7c053841edf5ebfbee9efbc08577e737397d0b
SeriousCoder <0kintol0@gmail.com>
2016-01-16 13:02:44 +0300

    - Added EqClasses

--- 2015/2/EqClasses.hs
+++ 2015/2/EqClasses.hs
@@ -1,8 +1,18 @@
 module EqClasses where
 
 import Rel
+import Data.List
 
 -- Написать функцию, которая по отношению (см. файл Rel.hs), которое
 -- является отношением эквивалентности, строит список классов эквивалентности
 classes :: R -> [[Int]]
-classes = undefined
+classes (R x) = foo x [] where
+	foo :: [(Int, Int)] -> [[Int]] -> [[Int]]
+	foo [] y 						= y 
+	foo (x:xs) y					= foo xs (incl x y)
+
+	incl :: (Int, Int) -> [[Int]] -> [[Int]]
+	incl x [] 		= [[fst x, snd x]]
+	incl x (y:ys) 	= if (elem (fst x) y) || (elem (snd x) y) 
+						then (nub ((fst x):(snd x):y)):ys
+						else y:(incl x ys)
