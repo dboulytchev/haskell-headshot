a7fab7c65022d084a9895019d2380c4930d1db2f
SergeevED <sergeev.ev.dm@gmail.com>
2016-01-16 02:19:18 -0800

    update

--- 2015/2/EqClasses.hs
+++ 2015/2/EqClasses.hs
@@ -2,16 +2,17 @@ module EqClasses where
 
 import Rel
 import Data.List
+import Tester
 
 -- Написать функцию, которая по отношению (см. файл Rel.hs), которое
 -- является отношением эквивалентности, строит список классов эквивалентности
 classes :: R -> [[Int]]
-classes (R l) = map nub . delete [] $ classes' l
+classes (R l) = map nub $ classes' l
 
 classes' :: [(Int,Int)] -> [[Int]]
 classes' l = 
 	case l of
-		[] -> [[]]
+		[] -> []
 		(a,b):k -> let (x,y) = findEq [a,b] k in
 			x : (classes' y)
 
@@ -32,3 +33,32 @@ findEq' ans olist nlist w =
 		qu@(q,u):r -> if (q `elem` ans || u `elem` ans) --(a == q || b == q || a == u || b == u) 
 					then findEq' (q:u:ans) r nlist True
 					else findEq' ans r (qu:nlist) w
+					
+rel :: Int -> IO ([Int], R)
+rel n = do
+  dom <- distinct topValue n
+  idx <- distinct n n
+  return (dom, R (zip dom (map ((dom!!) . (+ (negate 1))) idx)))
+
+tests :: [IO ()]
+tests = [
+         test "equivalent classes" (do
+                                 d <- distinct topValue 20
+                                 let c1 = take 5 d
+                                 let c2 = take 5 $ drop 5  d
+                                 let c3 = take 5 $ drop 10 d
+                                 let c4 = take 5 $ drop 15 d
+                                 let r1 = [(x, y) | x <- c1, y <- c1]
+                                 let r2 = [(x, y) | x <- c2, y <- c2]
+                                 let r3 = [(x, y) | x <- c3, y <- c3]
+                                 let r4 = [(x, y) | x <- c4, y <- c4]
+                                 let r  = R $ r1 ++ r2 ++ r3 ++ r4
+                                 let c  = classes r
+                                 putStr "Testing the number of equivalence classes ... "
+                                 assertResult (==) 4 $ length c
+                                 putStr "Testing the contents of equivalence classes ... "
+                                 assertResult (==) (sort $ map sort [c1, c2, c3, c4]) $ sort (map sort c)
+                                )
+        ]
+
+main = many tests
