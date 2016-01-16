2ca604614dff6d8189134b9e8e04f76d4fa43113
SlesarevIlya <slesarev.pr@gmail.com>
2016-01-16 14:37:21 +0300

    delete imp

--- 2015/2/EqClasses.hs
+++ 2015/2/EqClasses.hs
@@ -11,7 +11,7 @@ classes (R y) = main y [] where
   main (x: xs) t = main xs $ findClasses x t
 
   findClasses e []     = [[fst e, snd e]]
-  findClasses e (x:xs) = if ((elem (fst e) $ x) || (elem (snd e) $ x))
+  findClasses e (x:xs) = if ((elem (fst e) x) || (elem (snd e) x))
                       then (nub ((fst e):(snd e):x)):xs
                       else x:(findClasses e xs)
                       
