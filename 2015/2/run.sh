1dee506aa2a7ad962894b547ddfec7937c64eaa9
spolinaa <sokolova.polina@mail.ru>
2016-01-16 12:49:43 +0300

    Removed unnecessary tests

--- 2015/2/run.sh
+++ 2015/2/run.sh
@@ -2,20 +2,8 @@ function check () {
   if [ $? -ne 0 ] ; then exit 255 ; fi
 }
 
-ghc RelTest.hs
-check
-
-./RelTest
-check
-
 ghc EqClassesTest.hs
 check
 
 ./EqClassesTest
-check
-
-ghc ImpTest.hs
-check
-
-./ImpTest
 check
