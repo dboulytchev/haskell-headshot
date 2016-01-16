8600434f967364ada967e09ea7828c39e6a5b0db
spolinaa <sokolova.polina@mail.ru>
2016-01-16 13:31:17 +0300

    v.2

--- 2015/2/EqClasses.hs
+++ 2015/2/EqClasses.hs
@@ -6,19 +6,14 @@ import Rel
 -- является отношением эквивалентности, строит список классов эквивалентности
 classes :: R -> [[Int]]
 
-classes (R x) = makeList x [[]] where
+classes (R x) = makeList x [] where
     makeList :: [(Int, Int)] -> [[Int]] -> [[Int]]
     makeList [] y = y
     makeList (x:xs) y  = makeList xs (allLists y x1 x2)
              where 
                  (x1, x2) = x
-                 allLists [[]] x1 x2 |(x1 == x2) = [[x1]] 
-                                     | otherwise = [[x1, x2]]
-                 allLists [y]  x1 x2 | (elem x1 y), (not $ elem x2 y) = [x2:y] 
-                                     | (elem x2 y), (not $ elem x1 y) = [x1:y] 
-                                     | (elem x2 y), (elem x1 y)       = [y] 
-                                     | otherwise = y:(allLists [[]] x1 x2)
-                    
+                 allLists [] x1 x2 |(x1 == x2) = [[x1]] 
+                                   | otherwise = [[x1, x2]]
                  allLists (y:ys) x1 x2 | (elem x1 y), (not $ elem x2 y) = (x2:y):ys
                                        | (elem x2 y), (not $ elem x1 y) = (x1:y):ys
                                        | (elem x2 y), (elem x1 y)       = y:ys
