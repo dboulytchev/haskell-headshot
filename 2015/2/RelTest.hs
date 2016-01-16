5e2dfd5d60da18be098055b3a404baf55bf26372
Dmitry Boulytchev <dboulytchev@gmail.com>
2016-01-16 06:40:48 +0300

    Added 2nd exam files

--- 2015/2/RelTest.hs
+++ 2015/2/RelTest.hs
@@ -1,65 +1,12 @@
 import Data.List
-import System.Random
-import System.Exit
+import Tester
 import Rel
-
-topValue :: Int
-topValue = 1000
-
-rand :: Int -> IO Int
-rand n = getStdRandom $ randomR (1, n)
-
-distinct :: Int -> Int -> IO [Int]
-distinct k n | n > 20 = error $ "Non-observable test data requested: " ++ show n
-distinct k n = generate (return []) n where
-  generate acc 0 = acc
-  generate acc n = do
-    acc' <- acc 
-    k    <- rand k
-    if elem k acc' then generate acc n
-                   else generate (return $ k:acc') (n-1)
 
 rel :: Int -> IO ([Int], R)
 rel n = do
   dom <- distinct topValue n
   idx <- distinct n n
   return (dom, R (zip dom (map ((dom!!) . (+ (negate 1))) idx)))
-
-rep :: Int -> IO () -> IO ()
-rep 0 _ = return ()
-rep n f = do
-  _ <- f 
-  rep (n-1) f
-
-oops :: IO ()
-oops = do
-  putStrLn "***************"
-  putStrLn "*** Failure ***"
-  putStrLn "***************"
-  die ""
-
-test :: String -> IO () -> IO ()
-test name f = do
-  putStrLn header
-  putStrLn $ "*** Testing " ++ name ++ " ***"
-  putStrLn header
-  rep 10 f
-  putStrLn "***************"
-  putStrLn "*** Success ***"
-  putStrLn "***************" where
-    header = take (16 + length name) $ repeat '*'
-
-assertResult :: Show a => (a -> a -> Bool) -> a -> a -> IO ()
-assertResult f x y =
-  if f x y 
-  then putStrLn "ok" 
-  else do
-        putStrLn ""
-        putStrLn $ "Expected: " ++ show x ++ ", actual: " ++ show y
-        oops
-
-many :: [IO ()] -> IO ()
-many = foldl (>>) (return ()) 
 
 tests :: [IO ()]
 tests = [
@@ -108,7 +55,7 @@ tests = [
                         assertResult (==) False $ sub r'' r'                        
                      ),
           test "add" (do
-                        putStr "Testing add (R []) (R []) ... "
+                        putStr "Testing add (R []) (R []) ... "                      
                         assertResult (==) (R []) $ add (R []) (R [])
                         (_, r) <- rel 15
                         let r'  = R . take 7 . unR $ r
