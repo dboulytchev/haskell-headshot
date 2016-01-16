1b7ab4b22ffe76e872d4f0247e91ee999224d44c
Sharganov Artem <sharganov.artem@yandex.ru>
2016-01-16 13:37:37 +0300

    fix

--- 2015/2/EqClasses.hs
+++ 2015/2/EqClasses.hs
@@ -1,11 +1,11 @@
 module EqClasses where
 
+import Data.List
 import Rel
 
 -- Написать функцию, которая по отношению (см. файл Rel.hs), которое
 -- является отношением эквивалентности, строит список классов эквивалентности
-classes r = map nub $ foldl in_class [] (unR r)
-      where
+classes r = map nub $ foldl in_class [] (unR r) where
  	 in_class [] (a,b) = [[a, b]]
  	 in_class list@(y:ys) (a, b) = if (elem a y || elem b y) then (a:b:y):ys else [a, b]:list
 	 
