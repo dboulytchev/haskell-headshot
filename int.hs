2e71feeb3f0e185ee2b666735cf023ab47abe5b7
Your Name <sabrina.musatian@yandex.ru>
2017-01-23 16:39:37 +0300

    fix

--- int.hs
+++ int.hs
@@ -3,20 +3,10 @@ import qualified Data.Map as Map
 import System.Environment
 import Data.Maybe
 
--- регистр A
-data RegA = RegA {valA ::Int}
-
--- регистр D
-data RegD = RegD {valD :: Int}
-
--- тип инструкции
-data TypeOfCommand = E | R | J
-
 -- Команда в программе
-data Command = Command {flag :: Maybe String,
-                        com :: TypeOfCommand,
-                        number:: Maybe Int,
-                        flagToGo :: Maybe String}
+data Command = CommandE {flag :: Maybe String} |
+               CommandR {flag :: Maybe String} |
+               CommandJ {flag :: Maybe String , number :: Int, flagToGo :: String}
 
 -- Список команд
 type Instructions = [Command]
@@ -33,38 +23,16 @@ getCommand comm@(x:xs) = let isFlag s = if (last s == ':') then Just $ init s el
 makeComm :: Maybe String -> ProgramText -> Command
 makeComm lbl comm@(x:xs) =
   case x of
-      "e" -> Command lbl E Nothing Nothing
-      "r" -> Command lbl R Nothing Nothing
+      "e" -> CommandE lbl
+      "r" -> CommandR lbl
       "j" -> makeJump lbl comm where
-        makeJump lbl (j:n:nl) = Command lbl J (Just (read n :: Int)) (Just $ head nl)
+        makeJump lbl (j:n:nl) = CommandJ lbl (read n :: Int) (head nl)
 
 
 -- Парсит входную программу на команды
 parseInput :: String -> Instructions
 parseInput prog = makeListOfCommands $ lines prog where
      makeListOfCommands prog = foldr (\ x xs -> (getCommand $ words x) : xs) [] prog
-
-
-data Execution = Finished String |
-                 ReadStream {regD::RegD} |
-                 Jump {regAj :: RegA, regDj :: RegD, lbl :: Maybe String}
-
--- выполянет одну команду программы
-execComm :: Command -> RegA -> RegD -> Int -> Int -> Execution
-execComm comm curRega curRegd numStr maxStr =
-     case com comm of
-          E | (valA curRega == (-1) && numStr > maxStr)
-                  -> Finished $ show $ valD curRegd
-            | otherwise -> Finished "."
-
-          R | (valA curRega == (-1) && numStr <= maxStr)
-                  ->ReadStream curRegd
-            | otherwise ->  Finished "."
-
-          J | (valA curRega == (-1)) -> Finished "."
-            | (valA curRega == 0)
-                  -> Jump (RegA (-1)) (RegD (valD curRegd +  fromJust ( number comm))) (flagToGo comm )
-            | otherwise -> Jump (RegA (valA curRega - 1)) curRegd Nothing
 
 
 -- Делает словарь по лэйблам - (лэйблб, номер в списке команд)
@@ -83,12 +51,17 @@ main =
     let program = parseInput text
     let dict = makeDictOfLabels program
     let runTheComm numCom curRegA curRegD stStat =
-          case execComm (program !! numCom) curRegA curRegD stStat lenArgs of
-                       Finished x                     -> x
-                       ReadStream d                   ->
-                          runTheComm (numCom + 1) (RegA (read $ args !! stStat :: Int)) d (stStat + 1)
-                       Jump (RegA (-1)) d (Just lst)  ->
-                          runTheComm (fromJust $ Map.lookup lst dict) (RegA (-1)) d stStat
-                       Jump a d _                     -> runTheComm (numCom + 1) a d stStat
-    do putStrLn (runTheComm 0 (RegA (-1)) (RegD 0) 1)
+          case (program !! numCom)of
+
+            CommandE _  | (curRegA == Nothing && stStat > lenArgs) -> show curRegD
+                        | otherwise -> "."
+
+            CommandR _ | (curRegA == Nothing && stStat <= lenArgs) -> runTheComm (numCom + 1) (Just (read $ args !! stStat :: Int)) curRegD (stStat + 1)
+                       | otherwise -> "."
+
+            CommandJ _ num lst | (curRegA == Nothing) -> "."
+                               | (fromJust curRegA == 0) -> runTheComm (fromJust $ Map.lookup lst dict) (Nothing) (curRegD + num)  stStat
+                               | otherwise -> runTheComm (numCom + 1) (Just (fromJust curRegA - 1)) curRegD stStat
+
+    do putStrLn (runTheComm 0 Nothing 0 1)
        return ()
