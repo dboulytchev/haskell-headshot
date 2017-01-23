e35482c4fb026fa6dd7a4a6ce5ad6cb73ad7e78b
Irina Len <ia.len@mail.ru>
2017-01-23 16:08:17 +0300

    test1.1

--- int.hs
+++ int.hs
@@ -1,2 +1,12 @@
-import Main
-main = main
+import Parser
+import Exec
+import System.Environment
+
+
+main = do 
+	   args <- getArgs
+	   let path = args !! 0
+	   let input = tail args
+	   paersed <- Parser.parse path
+	   ex      <- Exec.result paersed input
+	   return ()
