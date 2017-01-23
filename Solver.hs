44e22be439212813348344189801c0fdb85c4ec3
chugaev <chugaev.tolya@gmail.com>
2017-01-23 19:10:32 +0400

    Add files via upload

--- Solver.hs
+++ Solver.hs
@@ -1,13 +1,9 @@
 module Solver where
 import Parse
 
--- solve a d inputCode instr
-solve Nothing d [] (E:instr) allInstr = Just d
-solve Nothing _ xs (E:instr) allInstr = Nothing
-solve Nothing _ [] (R:instr) allInstr = Nothing
-solve (Just _) _ _ (R:instr) allInstr = Nothing 
-solve Nothing d (x:xs) (R:instr) allInstr = solve (Just x) d xs instr allInstr
-solve (Just 0) d xs ((J n l):instr) allInstr = solve Nothing (d + n) xs (drop (l - 1) allInstr) allInstr
-solve Nothing d xs ((J n l):instr) allInstr = Nothing 
+solve :: (Ord a, Num a) => Maybe a -> Int -> [a] -> [Instr] -> [Instr] -> Maybe Int
+solve Nothing d [] (E:_) _                           = Just d
+solve Nothing d (x:xs) (R:instr) allInstr            = solve (Just x) d xs instr allInstr
+solve (Just 0) d xs ((J n l):instr) allInstr         = solve Nothing (d + n) xs (drop (l - 1) allInstr) allInstr
 solve (Just a) d xs ((J n l):instr) allInstr | a > 0 = solve (Just (a - 1)) d xs instr allInstr
-                                             | a < 0 = Nothing
+solve _ _ _ _ _                                      = Nothing
