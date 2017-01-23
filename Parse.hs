1b67957b2cb49f31009f3eeab791ba62b40a3628
chugaev <chugaev.tolya@gmail.com>
2017-01-23 19:06:02 +0400

    Add files via upload

--- Parse.hs
+++ Parse.hs
@@ -2,6 +2,7 @@ module Parse where
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe
+import Data.List.Split
 
 data Instr = R | E | J Int Int deriving (Show,Eq)
 type Text = [Instr]
@@ -10,11 +11,12 @@ parse :: FilePath -> IO Text
 parse path = do
     contents <- readFile path
     let labelMap = makeMap contents
-    return $ map (stringToInstr labelMap) (map (words . (dropPartList [])) (lines contents))
+    return $ map (stringToInstr labelMap) (map (words . (dropPartList)) (lines contents))
 
-dropPartList :: [Char] -> [Char] -> [Char]
-dropPartList ys []      =  ys
-dropPartList ys (x:xs) = if x == ':' then xs else dropPartList (ys ++ [x]) xs  
+dropPartList :: [Char] -> [Char]
+dropPartList xs = case splitOn ":" xs of
+    [x,y] -> y
+    [x]   -> x
 
 stringToInstr :: (Map [Char] Int) -> [[Char]] -> Instr
 stringToInstr labelMap [x]       = if (x == "r") then R else E
@@ -23,4 +25,4 @@ stringToInstr labelMap [x,y,z]   = J (toInt y) (fromJust (Map.lookup z labelMap)
 toInt s = read s :: Int
 
 makeMap :: (Num a, Enum a) => String -> Map [Char] a
-makeMap xs  =  Map.fromList $ map (\(x,y) -> (takeWhile  (\z -> z /= ':') x,y)) (filter (or . map (\x -> x == ':') . fst) (zip  (lines xs) [1..]))
+makeMap xs = Map.fromList $ map (\(x,y) -> (takeWhile (\z -> z /= ':') x,y)) (filter (or . map (\x -> x == ':') . fst) (zip (lines xs) [1..]))
