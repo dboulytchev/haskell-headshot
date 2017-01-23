aec2017e4694d60b0080a06549169779eaef3dc5
miha2733 <mishakosticyn@yandex.ru>
2017-01-24 02:50:14 +1100

    Exam

--- int.hs
+++ int.hs
@@ -1,50 +1,49 @@
 import Data.List
 import Data.Char
 import System.Environment
-data RegisterA = A Int | N deriving Eq
+
 data Flag = F String | NoFlag deriving Eq
-data Command = E | R | J Int Flag
-data Line = Line { flag :: Flag, command :: Command }
+data Command = E | R | J Int String
+data Line = Line { flag :: Flag, command :: Command } 
 data Result = Result Int | Interrupted
 
 instance Show Result where
-    show (Result a) = show a
-    show Interrupted = "."
-aToInt (A x) = x
-aToInt _ = -1
+    show (Result x) = show x
+    show _ = "."
 
-fromStringToCommand :: String -> Command
-fromStringToCommand xs = case head . dropSp $ xs of
-    'j' -> J (read . takeWhile (isDigit) . dropBegin $ xs) (F $ dropSp . dropWhile (isDigit) . dropBegin $ xs)
-    'r' -> R
-    'e' -> E
-    where 
-        dropSp = dropWhile (isSpace)
-        dropBegin = dropSp . drop 1 . dropSp
+aToInt (Just x) = x
+fromStringToCommand :: [String] -> Command        
+fromStringToCommand (x:(y:ys)) = case x of
+    "j" -> J (read y) (concat ys)
+    "r" -> R
+    "e" -> E
 
 fromStringToLine xs = Line { flag    = if null y then NoFlag else F x, 
-                             command = if null y then fromStringToCommand x else fromStringToCommand (tail y) }
+                             command = if null y then fromStringToCommand . words $ x else fromStringToCommand . words . tail $ y }
     where (x,y) = span (/= ':') xs
 
 seek [] y = []
 seek next@(x:xs) y | flag x == y = next
                    | otherwise = seek xs y 
 
-run :: [Line] -> [Line] -> [Int] -> RegisterA -> Int -> Result
+run :: [Line] -> [Line] -> [Int] -> Maybe Int -> Int -> Result
+run lns (cur:next) [] a d = case command cur of
+    R -> Interrupted
+    E -> if a == Nothing then Result d else Interrupted
+    J n l -> jumpHelper n (F l) a d
+        where jumpHelper n l a d | a == Just 0 = run lns (seek lns l) [] Nothing (d + n)
+                                 | a > Just 0 = run lns next [] (Just $ aToInt a - 1) d
+                                 | otherwise = Interrupted
 run lns (cur:next) stream a d = case command cur of
-    E -> if (a == N) && (null stream) then Result d else Interrupted
-    R -> if (a == N) && (not . null $ stream) then run lns next (tail stream) (A (head stream)) d else Interrupted
-    J n l -> jumpHelper n l a d
-        where jumpHelper n l a d | (aToInt a) < 0 = Interrupted
-                                 | (aToInt a) == 0 = run lns (seek lns l) stream N (d + n) -- point = l
-                                 | (aToInt a) > 0 = run lns next stream (A $ aToInt a - 1) d
-
-int :: IO ()
-int = do
+    E -> Interrupted
+    R -> if a == Nothing then run lns next (tail stream) (Just . head $ stream) d else Interrupted
+    J n l -> jumpHelper n (F l) a d
+        where jumpHelper n l a d | a == Just 0 = run lns (seek lns l) stream Nothing (d + n)
+                                 | a > Just 0 = run lns next stream (Just $ aToInt a - 1) d
+                                 | otherwise = Interrupted
+main = do
     args <- getArgs
     prog <- readFile (head args)
     let commands = lines prog
     let lns = map fromStringToLine commands
-    print (run lns lns (map read (tail args)) N 0)
-
-main = int
+    print (run lns lns (map read (tail args)) Nothing 0)
